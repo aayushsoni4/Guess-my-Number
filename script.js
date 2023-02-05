fe94c700d116a48c65d74c6c7516a4665fcfb105
Aayush <aayushsoni424@gmail.com>
2023-02-06 04:36:16 +0530

    Update script.js
    Introduced Enter key

--- script.js
+++ script.js
@@ -36,6 +36,38 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 })
 
+document.querySelector('input').addEventListener('keypress', function (e) {
+    if (e['key'] === "Enter") {
+        const guess = Number(document.querySelector('.guess').value);
+        if (!guess) {
+            displayMessage('No number');
+        }
+        if (guess === num) {
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem';
+            let highscore = Number(document.querySelector('.highscore').textContent);
+            document.querySelector('.highscore').textContent = Math.max(score, highscore);
+            displayMessage('Correct Number 🎉');
+            document.querySelector('.number').textContent = num;
+        }
+        else {
+            if (score > 1) {
+                displayMessage(guess > num ? 'Too high' : 'Too low');
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+            else {
+                displayMessage('You lost the game.');
+                document.querySelector('h1').textContent = "Number was";
+                document.querySelector('body').style.backgroundColor = "black"
+                document.querySelector('.number').textContent = num;
+                document.querySelector('.score').textContent = 0;
+            }
+        }
+    }
+});
+
+
 document.querySelector('.again').addEventListener('click', function () {
     num = Math.trunc(Math.random() * 30) + 1;
     document.querySelector('.score').textContent = 10;
